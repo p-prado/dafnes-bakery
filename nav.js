ce497f6ec55ba0be6a8c1c0865de5ced3132fe42
Pablo Prado <pradopablo502@gmail.com>
2023-03-07 17:17:54 -0600

    Make Homepage Responsive

--- nav.js
+++ nav.js
@@ -1,24 +1,9 @@
-const primaryNav = document.querySelector('.primary-navigation');
-const navToggle = document.querySelector('.mobile-nav-toggle');
-
-navToggle.addEventListener('click', ()=> {
-    const visibility = primaryNav.getAttribute('data-visible')
-    console.log(visibility);
-
-    if (visibility === "false") {
-        primaryNav.setAttribute('data-visible', true);
-        navToggle.setAttribute('aria-expanded', true);
-    } else if (visibility === "true"){
-        primaryNav.setAttribute('data-visible', false);
-        navToggle.setAttribute('aria-expanded', false);
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function makeNavMobile() {
+    var transparentNav = document.getElementById("transparent-navigation");
+    if (transparentNav.className === "transparent-navigation") {
+      transparentNav.className += " responsive";
+    } else {
+      transparentNav.className = "transparent-navigation";
     }
-})
-
-// CHANGE NAV COLOR ON SCROLL
-// document.querySelector(window).scroll(function() {
-//     if (document.querySelector(this).scrollTop > 400) {
-//         document.querySelector( ".header #background" ).fadeIn();
-//     } else {
-//         document.querySelector( ".header #background" ).fadeOut();
-//     }
-// });
+  }
