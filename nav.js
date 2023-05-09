5975a54c1b96535d57e0fed2f09b60f21164d45e
Pablo Prado <pradopablo502@gmail.com>
2023-05-09 13:28:37 -0600

    Resctructure Files

--- nav.js
+++
@@ -1,9 +0,0 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function makeNavMobile() {
-    var transparentNav = document.getElementById("transparent-navigation");
-    if (transparentNav.className === "transparent-navigation") {
-      transparentNav.className += " responsive";
-    } else {
-      transparentNav.className = "transparent-navigation";
-    }
-  }
